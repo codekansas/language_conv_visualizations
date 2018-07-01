78a9324c5490926e44f30aec7f08103a83557c1e
Benjamin Bolte <bbolte@fb.com>
2018-07-01 16:51:15 -0700

    added typign

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from tensorflow import keras as ks
 import utils
 
 
-def train(embed_size, batch_size, epochs):
+def train(embed_size: int, batch_size: int, epochs: int) -> None:
     dataset = utils.Dataset()
 
     model = utils.build_model(
@@ -28,17 +28,26 @@ def train(embed_size, batch_size, epochs):
         batch_size=32,
         epochs=epochs,
         validation_data=[dataset.x_test, dataset.y_test],
+        callbacks=[
+            ks.callbacks.ModelCheckpoint(
+                filepath='model.h5',
+                monitor='loss',
+                save_best_only=True,
+                save_weights_only=True,
+            ),
+        ],
     )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script for training a model')
     parser.add_argument('-e', '--embedding-size', type=int, default=128,
-        help='Number of dimensions in the word embeddings')
+                        help='Number of dimensions in the word embeddings')
     parser.add_argument('-n', '--num-epochs', type=int, default=10,
-        help='Number of epochs for which to fit the model to the dataset')
+                        help='Number of epochs for which to fit the model')
     parser.add_argument('-b', '--batch-size', type=int, default=32,
-        help='Batch size for training the network')
+                        help='Batch size for training the network')
+
     args = parser.parse_args()
     train(
         embed_size=args.embedding_size,
--- utils.py
+++ utils.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import List, Tuple, TypeVar
 
 from tensorflow import keras as ks
 
+# Defines some types that are used in various places.
+Matrix = TypeVar('Matrix')
+DataPair = Tuple[Tuple[Matrix, Matrix], Tuple[Matrix, Matrix]]
 
-def build_model(sequence_len, vocab_size, embed_size):
+
+def build_model(sequence_len: int,
+                vocab_size: int,
+                embed_size: int) -> ks.models.Model:
     i = ks.layers.Input(shape=(sequence_len,))
     x = ks.layers.Embedding(vocab_size, embed_size, name='embeddings')(i)
-    x = ks.layers.Conv1D(1000, 3, activation='relu')(x)
+    x = ks.layers.Conv1D(1000, 3, name='convs', use_bias=False)(x)
+    x = ks.layers.Activation('relu')
+    x = ks.layers.BatchNormalization(name='batch_norm')(x)
     x = ks.layers.GlobalAveragePooling1D()(x)
-    x = ks.layers.Dense(1, activation='sigmoid')(x)
+    x = ks.layers.Dense(1)(x)
+    x = ks.layers.Activation('relu')
     return ks.models.Model(inputs=[i], outputs=[x])
 
 
 class Dataset(object):
-    def __init__(self, sequence_len=400):
+    def __init__(self, sequence_len: int=400) -> None:
         self.word_to_index = ks.datasets.imdb.get_word_index()
         self.index_to_word = {v: k for k, v in self.word_to_index.items()}
         self.sequence_len = sequence_len
@@ -30,7 +39,7 @@ class Dataset(object):
         ]
 
     @property
-    def data(self):
+    def data(self) -> DataPair:
         if not hasattr(self, '_x_train'):
             (x_train, y_train), (x_test, y_test) = ks.datasets.imdb.load_data()
             x_train = ks.preprocessing.sequence.pad_sequences(
@@ -42,19 +51,19 @@ class Dataset(object):
         return (self._x_train, self._y_train), (self._x_test, self._y_test)
 
     @property
-    def x_train(self): return self.data[0][0]
+    def x_train(self) -> Matrix: return self.data[0][0]
 
     @property
-    def y_train(self): return self.data[0][1]
+    def y_train(self) -> Matrix: return self.data[0][1]
 
     @property
-    def x_test(self): return self.data[1][0]
+    def x_test(self) -> Matrix: return self.data[1][0]
 
     @property
-    def y_test(self): return self.data[1][1]
+    def y_test(self) -> Matrix: return self.data[1][1]
 
     @property
-    def vocab_size(self):
+    def vocab_size(self) -> int:
         if not hasattr(self, '_vocab_size'):
             self._vocab_size = max(
                 self.x_train.max(), self.y_train.max(),
